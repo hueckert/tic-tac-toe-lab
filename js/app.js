fa0220918f77f63d634697c1fa02c58794926e02
hueckert <howard.ueckert@protonmail.com>
2024-08-14 22:50:55 -0500

    add vari and const

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const squareEls = document.querySelectorAll(".sqr")
+const messageEl = document.querySelector("#message")
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board 
+let turn 
+let winner 
+let tie 
 
 
 /*------------------------ Cached Element References ------------------------*/
